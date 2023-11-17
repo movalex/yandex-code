3429da6d8217b06458eb9ab293f4eabb8a7c598b
Alexey Bogomolov <mail@abogomolov.com>
2023-11-17 22:52:04 +0300

    add tests

--- most_common_words.py
+++ most_common_words.py
@@ -1,5 +1,11 @@
+import unittest
+import re
+from typing import List
+from collections import Counter
+
+
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+    def most_common_word(self, paragraph: str, banned: List[str]) -> str:
         """
         Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
         Output: "ball"
@@ -12,8 +18,31 @@ class Solution:
         """
         paragraph = re.sub("[.,!?;']", " ", paragraph.lower())
         count = Counter(paragraph.split())
-        mc = count.most_common(2)
+        mc = count.most_common()
 
         for word, num in mc:
             if word not in banned:
                 return word
+
+
+class TestFormatting(unittest.TestCase):
+    def setUp(self):
+        self.func = Solution()
+
+    def test_formatting1(self):
+        paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+        banned = ["hit"]
+        actual = self.func.most_common_word(paragraph, banned)
+        expected = "ball"
+        self.assertEqual(actual, expected)
+
+    def test_formatting2(self):
+        paragraph = "Bob bAlL, hIt. ball Int int hit!"
+        banned = ["ball", "int"]
+        actual = self.func.most_common_word(paragraph, banned)
+        expected = "hit"
+        self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
